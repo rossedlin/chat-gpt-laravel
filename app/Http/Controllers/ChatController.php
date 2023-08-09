1cfa61507ae5fa635e4b8d29a187cd15f035fdb8
Ross Edlin <rossedlin@gmail.com>
2023-08-09 15:18:36 +0100

    Fix Chat GPT Limit

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,33 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Throwable;
 
 class ChatController extends Controller
 {
     /**
      * @param Request $request
-     * @return JsonResponse
+     * @return string
      */
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(Request $request): string
     {
-        $response = Http::withHeaders([
-            "Content-Type" => "application/json",
-            "Authorization" => "Bearer " . env('CHAT_GPT_KEY')
-        ])->post('https://api.openai.com/v1/chat/completions', [
-            "model" => $request->post('model'),
-            "messages" => [
-                [
-                    "role" => "user",
-                    "content" => $request->post('content')
-                ]
-            ],
-            "temperature" => 0,
-            "max_tokens" => 2048
-        ])->body();
 
-        return response()->json(json_decode($response));
+        try {
+            /** @var array $response */
+            $response = Http::withHeaders([
+                "Content-Type" => "application/json",
+                "Authorization" => "Bearer " . env('CHAT_GPT_KEY')
+            ])->post('https://api.openai.com/v1/chat/completions', [
+                "model" => $request->post('model'),
+                "messages" => [
+                    [
+                        "role" => "user",
+                        "content" => $request->post('content')
+                    ]
+                ],
+                "temperature" => 0,
+                "max_tokens" => 2048
+            ])->body();
+            return $response['choices'][0]['message']['content'];
+        } catch (Throwable $e) {
+            return "Chat GPT Limit Reached. This means too many people have used this demo this month and hit the FREE limit available. You will need to wait, sorry about that.";
+        }
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
       //Populate receiving message
       $(".messages > .message").last().after('<div class="left message">' +
         '<img src="https://assets.edlin.app/images/rossedlin/03/rossedlin-03-100.jpg" alt="Avatar">' +
-        '<p>' + res.choices[0].message.content + '</p>' +
+        '<p>' + res + '</p>' +
         '</div>');
 
       //Cleanup
